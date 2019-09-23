2e45ac7a7b2e85c09a681f395140b3b5ab971965
Caeden Meade <cmeade3@gatech.edu>
2019-09-23 07:43:42 -0400

    Working on dropdown implementation

--- alignments/forms.py
+++ alignments/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from .models import Taxgroups
 
 class TaxgroupForm(forms.ModelForm):
+
+    
     class Meta:
         model = Taxgroups
         fields = ('superkingdom', 'phyla', 'alignment')
