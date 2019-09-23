529f968a1210c202b9729db8996f244c6d9fbefc
Caeden Meade <cmeade3@gatech.edu>
2019-09-23 05:52:44 -0400

    Working on dropdown implementation

--- alignments/forms.py
+++ alignments/forms.py
@@ -1,11 +1,11 @@
-from django import forms
-from .models import Taxgroups
+# from django import forms
+# from .models import Taxgroups
 
-class TaxgroupsForm(forms.ModelForm):
-    class Meta:
-        model = Taxgroups
-        fields = ('superkingdom', 'phyla', 'alignment')
+# class TaxgroupsForm(forms.ModelForm):
+#     class Meta:
+#         model = Taxgroups
+#         fields = ('superkingdom', 'phyla', 'alignment')
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['superkingdom'].queryset = Taxgroups.objects.none()
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.fields['superkingdom'].queryset = Taxgroups.objects.none()
