2d81af34010c1d02af24fa81a6e8f48191c79970
Caeden Meade <cmeade3@gatech.edu>
2019-09-23 03:47:09 -0400

    Working on suprkingdom filtration

--- alignments/views.py
+++ alignments/views.py
@@ -61,8 +61,7 @@ def build_alignment(rawMYSQLresult):
 
 def index(request):
 	some_Alignments = Alignment.objects.all()
-	superKingdoms = Taxgroups.objects.raw('SELECT * FROM SEREB.TaxGroups\
-		 WHERE SEREB.TaxGroups.groupLevel = "superkingdom"')
+	superKingdoms = Taxgroups.objects.raw('SELECT * FROM SEREB.TaxGroups')
 	context = {
 		'some_Alignments': some_Alignments,
 		'superKingdoms': superKingdoms
