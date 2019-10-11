24433bb15b85a91743bfbbb6e4b9847f56ae4f66
Petar Penev <ppenev@gatech.edu>
2019-10-10 23:08:41 -0400

    Decoding the residue request data.

--- ribovision/views.py
+++ ribovision/views.py
@@ -14,12 +14,13 @@ def fetchresidues(request):
         structure_identity = request.body
         SQLStatement = 'SELECT ss.map_Index, ss.molName, resNum, X, Y, unModResName, modResName, MoleculeType, \
                     MoleculeGroup, ChainName, Domain_RN, Domain_AN, Domains_Color, Helix_Num, Helix_Color, ss.SS_Table \
-                    FROM (SELECT * FROM SecondaryStructures WHERE SS_Table = '+structure_identity+') AS ss \
+                    FROM (SELECT * FROM SecondaryStructures WHERE SS_Table = '+structure_identity.decode()+') AS ss \
                     INNER JOIN MoleculeNames AS mn ON ss.molName = mn.MoleculeName \
                     INNER JOIN (SELECT MoleculeName, ChainName FROM ChainList \
-                    WHERE StructureName = (SELECT DISTINCT StructureName FROM Secondary_Tertiary WHERE SS_Table = '+structure_identity+')) \
+                    WHERE StructureName = (SELECT DISTINCT StructureName FROM Secondary_Tertiary WHERE SS_Table = '+structure_identity.decode()+')) \
                     AS cl ON cl.MoleculeName = ss.molName \
-                    LEFT JOIN (SELECT * FROM StructuralData2 WHERE SS_Table = '+structure_identity+') AS sd ON sd.map_Index = ss.map_Index'
+                    LEFT JOIN (SELECT * FROM StructuralData2 WHERE SS_Table = '+structure_identity.decode()+') AS sd ON sd.map_Index = ss.map_Index'
+    
     return JsonResponse(list(SQLStatement), safe = False)
 
 def index(request):
