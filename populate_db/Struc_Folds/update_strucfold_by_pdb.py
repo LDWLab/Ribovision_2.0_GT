f8f54b00ec7264d817de0fa33ca2d374f1a6b669
Petar Penev <ppenev@gatech.edu>
2020-04-16 16:31:39 -0400

    Completed upload script for Strucfold + chain resi

--- populate_db/Struc_Folds/update_strucfold_by_pdb.py
+++ populate_db/Struc_Folds/update_strucfold_by_pdb.py
@@ -41,18 +41,73 @@ def upload_struc_fold(level, name, class_sys, parent, external_id, cursor, cnx):
 		query = "INSERT INTO `SEREB`.`Structural_Folds`(`Level`,`Name`,`classification_system`,`parent`,`external_id`) VALUES('"+level+"','"+name+"','"+class_sys+"','"+parent+"','"+external_id+"')"
 	else:
 		query = "INSERT INTO `SEREB`.`Structural_Folds`(`Level`,`Name`,`classification_system`,`external_id`) VALUES('"+level+"','"+name+"','"+class_sys+"','"+external_id+"')"
-	print(query)
 	cursor.execute(query)
 	lastrow_id = str(cursor.lastrowid)
 	cnx.commit()
 	return lastrow_id
 
-def check_then_upload_struc_fold(ecod_definitions, cursor, cnx):
+def upload_strucfold_chains(cursor, strucfoldid, chainid):
+	cursor.execute("SELECT * FROM SEREB.StrucFold_Chains WHERE\
+					strucfold_id = '"+strucfoldid+"' AND\
+					chain_id = '"+chainid+"'")
+	result = cursor.fetchall()
+	if len(result) == 0:
+		query = "INSERT INTO `SEREB`.`StrucFold_Chains`(`strucfold_id`, `chain_id`) VALUES ('"+strucfoldid+"','"+chainid+"')"
+		cursor.execute(query)
+	return True
+
+def upload_strucfold_resis(cursor, strucfoldid, resi_ids):
+	for strucid, resid in map(lambda e: (e, strucfoldid), resi_ids):
+		cursor.execute("SELECT * FROM SEREB.StrucFold_Residues WHERE\
+						strucfold_id = '"+str(strucid)+"' AND\
+						residue_id = '"+str(resid)+"'")
+		result = cursor.fetchall()
+		if len(result) == 0:
+			query = "INSERT INTO `SEREB`.`StrucFold_Residues`(`residue_id`, `strucfold_id`) VALUES ('"+str(strucid)+"','"+str(resid)+"')"
+			cursor.execute(query)
+	return True
+
+def get_resis(cursor, pol_id, resi_ranges):
+	between_statement = ""
+	if len(resi_ranges.split(",")) == 1:
+		resirange1 = resi_ranges.split(":")[1].split("-")[0]
+		resirange2 = resi_ranges.split(":")[1].split("-")[1]
+		between_statement = "SEREB.Residues.resNum BETWEEN "+resirange1+" AND "+resirange2
+	if len(resi_ranges.split(",")) > 1:
+		list_betweens = list()
+		for one_range in resi_ranges.split(","):
+			resirange1 = one_range.split(":")[1].split("-")[0]
+			resirange2 = one_range.split(":")[1].split("-")[1]
+			list_betweens.append("SEREB.Residues.resNum BETWEEN "+resirange1+" AND "+resirange2)
+		between_statement = " OR ".join(list_betweens)
+	
+	cursor.execute("SELECT resi_id FROM SEREB.Residues WHERE\
+					SEREB.Residues.PolData_id = "+pol_id+" AND ("+between_statement+")")
+	result = cursor.fetchall()
+	return [item for sublist in result for item in sublist]
+
+def check_polymerid_from_chainid(cursor, pdbid, chainid):
+	cursor.execute("SELECT polymer_id, ChainList_id FROM SEREB.ChainList\
+					INNER JOIN SEREB.ThreeDStructures ON SEREB.ChainList.3D_structure_id = SEREB.ThreeDStructures.3D_structure_id\
+					WHERE SEREB.ThreeDStructures.StructureName = '"+pdbid.upper()+"' AND\
+					SEREB.ChainList.ChainName = '"+chainid+"'")
+	result = cursor.fetchall()
+	if len(result) == 0:
+		return None, None
+	if len(result) == 1:
+		return result[0][0], result[0][1]
+	if len(result) > 1:
+		raise ValueError("Mistake in database for query with pdb "+pdbid+" and chain "+chainid+"!")
+
+def check_then_upload_struc_fold(ecod_definitions, cursor, cnx, pdbid):
 	levels = ['Architecture', 'X', 'H', 'T', 'F']
 	for entry_for_upload in ecod_definitions:
+		if re.match(r'UNCLASSIFIED', entry_for_upload[8]):
+			continue
 		parent = "NULL"
 		last_statement = "IS "+parent
 		external_ecod_id = ['0'] + entry_for_upload[0].split('.')
+		###------Separate this into function
 		for fold_name,fold_level,fold_id in zip(entry_for_upload[-5:], levels, external_ecod_id):
 			fold_name = fold_name.replace("\"","")
 			if fold_level != 'Architecture':
@@ -67,19 +122,28 @@ def check_then_upload_struc_fold(ecod_definitions, cursor, cnx):
 				parent = upload_struc_fold(fold_level, fold_name, 'ECOD', parent, fold_id, cursor, cnx)
 			if len(result) == 1:
 				parent = str(result[0][0])
-			
+		###------Up to here
+		pol_id, chain_id = check_polymerid_from_chainid(cursor, pdbid, entry_for_upload[1])
+		if chain_id is not None:
+			upload_strucfold_chains(cursor, str(parent), str(chain_id))
+		if pol_id is not None:
+			resi_ids_for_upload = get_resis(cursor, str(pol_id), entry_for_upload[2])
+			if len(resi_ids_for_upload) > 0:
+				upload_strucfold_resis(cursor, str(parent), resi_ids_for_upload)
 
 def main(commandline_arguments):
 	comm_args = create_and_parse_argument_options(commandline_arguments)
+	pdbid = str(comm_args.pdb_id).lower()
 	if comm_args.download_most_recent_fold_definitions:
 		download_latest_fold_defs('http://prodata.swmed.edu/ecod/distributions/ecod.latest.domains.txt', 'ecod.latest.domains.txt')
-	ecod_defs = parse_ecod_definitions(str(comm_args.pdb_id).lower())
+	ecod_defs = parse_ecod_definitions(pdbid)
 
 	cnx = initiate_connection(comm_args.user_name, '130.207.36.75', 'SEREB')
 	cursor = cnx.cursor()
 
-	check_then_upload_struc_fold(ecod_defs, cursor, cnx)
-
+	check_then_upload_struc_fold(ecod_defs, cursor, cnx, pdbid)
+	
+	cnx.commit()
 	cursor.close()
 	cnx.close()
 
