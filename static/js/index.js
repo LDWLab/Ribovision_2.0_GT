fbfb35a0297a74503a9f0e6de86a04b4604add5e
Petar Penev <ppenev@gatech.edu>
2020-09-04 16:48:52 -0400

    Getting topology viewer to work through Node.js

--- static/js/index.js
+++ static/js/index.js
@@ -2,62 +2,7 @@ import { actions, MSAViewer, SequenceViewer } from '@plotly/react-msa-viewer';
 import React, { Component } from "react";
 import ReactDOM, { render } from 'react-dom';
 //import PdbTopologyViewer from 'pdb-topology-viewer/src/web-component/index'
-import PdbTopologyViewerPlugin from 'pdb-topology-viewer/build/pdb-topology-viewer-plugin'
-
-function MyTopView() {
-  return (
-    class PdbTopologyViewer extends HTMLElement {
-
-      static get observedAttributes() {
-        return ['entry-id', 'entity-id', 'chain-id', 'display-style', 'error-style', 'menu-style', 'subscribe-events'];
-      }
-
-      constructor() {
-        super();
-      }
-
-      validateParams() {
-        if(typeof this.entryId == 'undefined' || typeof this.entityId == 'undefined' || this.entryId == null || this.entityId == null) return false;
-        return true
-      }
-
-      invokePlugin(){
-        let paramValidatity = this.validateParams();
-        if(!paramValidatity) return
-
-        // create an instance of the plugin
-        if(typeof this.pluginInstance == 'undefined') this.pluginInstance = new PdbTopologyViewerPlugin();
-
-        let options = {
-          entryId: this.entryId,
-          entityId: this.entityId,
-        }
-
-        if(typeof this.chainId !== 'undefined' && this.chainId !== null) options['chainId'] = this.chainId;
-        if(typeof this.displayStyle !== 'undefined' && this.displayStyle !== null) options['displayStyle'] = this.displayStyle;
-        if(typeof this.errorStyle !== 'undefined' && this.errorStyle !== null) options['errorStyle'] = this.errorStyle;
-        if(typeof this.menuStyle !== 'undefined' && this.menuStyle !== null) options['menuStyle'] = this.menuStyle;
-        if(typeof this.subscribeEvents !== 'undefined' && this.subscribeEvents !== null) options['subscribeEvents'] = this.subscribeEvents;
-
-        this.pluginInstance.render(this, options);
-
-      }
-
-      attributeChangedCallback() {
-        this.entryId = this.getAttribute("entry-id");
-        this.entityId = this.getAttribute("entity-id");
-        this.chainId = this.getAttribute("chain-id");
-        this.displayStyle = this.getAttribute("display-style");
-        this.errorStyle = this.getAttribute("error-style");
-        this.menuStyle = this.getAttribute("menu-style");
-        this.subscribeEvents = this.getAttribute("subscribe-events");
-        this.invokePlugin();
-      }
-
-    }
-  )
-}
-
+import {PdbTopologyViewerPlugin} from 'pdb-topology-viewer/build/pdb-topology-viewer-plugin-2.0.0'
 
 const options = {
   sequences: [
@@ -231,8 +176,3 @@ function MyMSA() {
   }
   return <SimpleTooltip />;
 };
-
-
-
-ReactDOM.render(<MyMSA />, document.getElementById("target"));
-ReactDOM.render(<MyTopView />, document.getElementById("target"));
