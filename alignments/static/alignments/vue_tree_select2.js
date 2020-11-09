8490e427ec31968fc6d3a4658fbcc40a60ae6c0e
Petar Penev <peteripenev@gmail.com>
2020-11-08 23:55:58 -0500

    Fixed scrollbars, dynamic resizing msaViewer.

--- alignments/static/alignments/vue_tree_select2.js
+++ alignments/static/alignments/vue_tree_select2.js
@@ -622,10 +622,11 @@ var vm = new Vue({
                 if (fasta['TwinCons'] != null){
                     this.custom_aln_twc_flag = fasta['TwinCons']
                 }
-                vm.fastaSeqNames = fasta['Sequence names']
-                var main_elmnt = document.querySelector(".alignment_section")
+                vm.fastaSeqNames = fasta['Sequence names'];
+                window.aaFreqs = fasta['AA frequencies'];
+                var main_elmnt = document.querySelector(".alignment_section");
                 window.main_elmnt = main_elmnt;
-                let seqsForMSAViewer = parseFastaSeqForMSAViewer(fasta['Alignment'])
+                let seqsForMSAViewer = parseFastaSeqForMSAViewer(fasta['Alignment']);
                 var msaOptions = {
                     sequences: seqsForMSAViewer,
                     colorScheme: "clustal2",
@@ -634,14 +635,14 @@ var vm = new Vue({
                     tileHeight: 18,
                     tileWidth: 18,
                     overflow: "auto",
-                }
+                };
                 window.msaOptions = msaOptions;
                 ReactDOM.render(
                     React.createElement(MyMSA, msaOptions),
                     document.getElementById('alnDiv')
                   );
                 this.fasta_data = fasta['Alignment'];
-                this.aa_properties = calculateFrequencyData(fasta['AA frequencies'])
+                this.aa_properties = calculateFrequencyData(fasta['AA frequencies']);
             })
         }, showTopologyViewer (pdbid, chainid, fasta){
             if (document.querySelector("pdb-topology-viewer") || document.querySelector("pdbe-molstar")) {cleanupOnNewAlignment(vm);}
@@ -833,7 +834,27 @@ var Tooltip = function (props) {
 window.ajaxRun = false;
 function MyMSA() {
     class SimpleTooltip extends React.Component {
-        state = { tileWidth: 18, tileHeight: 18, aaPos: 0, seqPos: 0, highlight: null };
+        state = { 
+            tileWidth: 18,
+            tileHeight: 18,
+            aaPos: 0,
+            seqPos: 0,
+            width: main_elmnt.offsetWidth * 0.7,
+            height: main_elmnt.offsetHeight * 0.9,
+            highlight: null 
+        };
+        handleResize = () => {
+            this.setState({
+                width: main_elmnt.offsetWidth * 0.7,
+                height: main_elmnt.offsetHeight * 0.9
+            })
+        };
+        componentDidMount() {
+            window.addEventListener("resize", this.handleResize);
+        };
+        componentWillUnmount() {
+            window.removeEventListener("resize", this.handleResize);
+        };
         onResidueMouseEnter = e => {
             if (vm.topology_loaded == 'True'){
                 let resiPos = vm.structure_mapping[e.position];
@@ -883,18 +904,20 @@ function MyMSA() {
         render() {
             const xPos = this.state.tileWidth * (this.state.aaPos - 1);
             const yPos = this.state.tileHeight * (this.state.seqPos - 1);
+            const maxXpos = window.aaFreqs.length - ((main_elmnt.offsetWidth * 0.7)/this.state.tileWidth);
+            const maxYpos = vm.fastaSeqNames.length - ((main_elmnt.offsetHeight * 0.9)/this.state.tileHeight);
             return (
             <div style={{ display: "flex" }}>
                 <div>
                   <input
                     style = {{ 
-                        width: main_elmnt.offsetWidth * 0.75+"px",
+                        width: main_elmnt.offsetWidth * 0.7+"px",
                         position: "relative",
                         left: main_elmnt.offsetWidth * 0.2+"px"
                     }}
                     type="range"
                     min="0"
-                    max="40" //Needs position on the left edge of the viewer. len(aln) - len(aas in viewport)
+                    max={maxXpos}
                     value={this.state.aaPos}
                     onChange={(evt) => this.setState({ aaPos: evt.target.value })}
                     class="slider"
@@ -904,6 +927,8 @@ function MyMSA() {
                 {...msaOptions}
                 ref={(ref) => (this.el = ref)}
                 highlight={this.state.highlight}
+                width={this.state.width}
+                height={this.state.height}
                 tileWidth={this.state.tileWidth}
                 tileHeight={this.state.tileHeight}
                 position={{ xPos, yPos }}
@@ -940,11 +965,11 @@ function MyMSA() {
             </div>
             <input
                 style={{ 
-                    width: main_elmnt.offsetHeight*0.8+"px",
+                    width: main_elmnt.offsetHeight*0.9+"px",
                 }}
                 type="range"
                 min="0"
-                max="40" //Needs position on the top edge of the viewer. total(seqs) - len(seqs in viewport)
+                max={maxYpos}
                 value={this.state.seqPos}
                 onChange={(evt) => this.setState({ seqPos: evt.target.value })}
                 class="slider"
