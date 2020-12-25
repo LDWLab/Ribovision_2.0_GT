fa8cc20f4d6d3d7e02bbf1ddb35fbde1fac58f71
Petar Penev <peteripenev@gmail.com>
2020-12-25 17:15:34 -0500

    Fix broken masking from previous commit

--- pdb-topology-viewer/build/pdb-topology-viewer-plugin.js
+++ pdb-topology-viewer/build/pdb-topology-viewer-plugin.js
@@ -1335,14 +1335,12 @@ var PdbTopologyViewerPlugin = /** @class */ (function () {
                     }];
                 var name = index;
                 var separatedData = value;
-                if (!selectSections_RV1.get(name)) {
-                    selectSections_RV1.set(name, []);
-                    var min = Math.min.apply(Math, aaPropertyConstants.get(name));
-                    var max = Math.max.apply(Math, aaPropertyConstants.get(name));
-                    var colormapArray = aaColorData.get(name);
-                    var _a = _this.parseTWCData(separatedData, min, max, colormapArray), TWCrgbMap = _a[0], TWCData = _a[1];
-                    selectSections_RV1.get(name).push({ entity_id: _this.entityId, focus: true });
-                }
+                selectSections_RV1.set(name, []);
+                var min = Math.min.apply(Math, aaPropertyConstants.get(name));
+                var max = Math.max.apply(Math, aaPropertyConstants.get(name));
+                var colormapArray = aaColorData.get(name);
+                var _a = _this.parseTWCData(separatedData, min, max, colormapArray), TWCrgbMap = _a[0], TWCData = _a[1];
+                selectSections_RV1.get(name).push({ entity_id: _this.entityId, focus: true });
                 if (void 0 !== TWCData) {
                     residueDetails = _this.create2D3DAnnotations(name, residueDetails, TWCrgbMap, TWCData, chainRange.start, chainRange.end);
                     if (0 < residueDetails.length) {
