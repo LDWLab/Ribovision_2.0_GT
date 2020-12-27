f15cffc3535e30e55d831ddb536d7f27bd98bcb6
Petar Penev <peteripenev@gmail.com>
2020-12-27 14:30:53 -0500

    separating PDB methods from ajaxfunc

--- static/js/components/testingPDB.js
+++ static/js/components/testingPDB.js
@@ -1,3 +1,4 @@
+import {ajaxProper} from './ajaxProper.js'
 export function testingCIFParsing (pdbID, entityIDS){
     vm.postedPDBEntities = false;
     let parseURL = `custom-struc-data/${pdbID}`;
@@ -13,41 +14,4 @@ export function testingCIFParsing (pdbID, entityIDS){
     }).catch(error => {
         console.log(error);
     });
-}
-
-var ajaxProper = function ({url, type, dataType, postData}={}){
-    if (type === 'POST'){
-        return new Promise((resolve, reject) => {
-            $.ajax({
-                url: url,
-                type: type,
-                dataType: dataType,
-                data: postData,
-                traditional: true,
-                headers: {'X-CSRFToken': csrftoken},
-                success: function(data) {
-                    resolve(data)
-                },
-                error: function(error) {
-                    console.log(`Error ${error}`);
-                    reject(error)
-                }
-            })
-        })
-    } else if (type === 'GET'){
-        return new Promise((resolve, reject) => {
-            $.ajax({
-                url: url,
-                type: type,
-                dataType: dataType,
-                success: function(data) {
-                    resolve(data)
-                },
-                error: function(error) {
-                    console.log(`Error ${error}`);
-                    reject(error)
-                }
-            })
-        })
-    }
 }
