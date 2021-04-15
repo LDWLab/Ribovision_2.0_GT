322594fc7dbe10ea4d41b105301cc952443c886e
Petar Penev <ppenev@apollo2.chemistry.gatech.edu>
2021-04-14 22:57:51 -0400

    Merge remote branch 'origin/dev' into public

--- static/js/components/DropDownTree.vue
+++ static/js/components/DropDownTree.vue
@@ -724,7 +724,7 @@
                 var range_string = minIndex.concat("-").concat(maxIndex);
                 let ebiMappingURL = 'https://www.ebi.ac.uk/pdbe/api/mappings/uniprot/'+pdbid;
                 ajax(ebiMappingURL).then(data=>{
-                    if (vm.topology_loaded){return;}
+                    if (vm.topology_loaded&&vm.topology_loaded!='error'){return;}
                     var result = [];
                     customFilter(data, result, "chain_id", chainid);
                     result = result[0];
