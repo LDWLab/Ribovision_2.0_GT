9f45a580efa353e2d7530e6d4caab62cfe9f0208
Petar Penev <peteripenev@gmail.com>
2021-04-29 20:09:52 -0400

    mv to right dir for upload

--- populate_db/Struc_Folds/updateECODrest.sh
+++ populate_db/Struc_Folds/updateECODrest.sh
@@ -5,6 +5,7 @@ wget -O ./ecod.latest.domains.txt "http://prodata.swmed.edu/ecod/distributions/e
 
 sed -i 's/,/;/g' ./ecod.latest.domains.txt
 sed -i 's/\t/,/g' ./ecod.latest.domains.txt
+mv ./ecod.latest.domains.txt /var/lib/mysql-files/ecod.latest.csv
 
 echo "Uploading data to MYSQL"
-mysql -h "130.207.36.76" SEREB_test -e "LOAD DATA INFILE './ecod.latest.domains.txt' INTO TABLE SEREB.EcodDomains FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n' IGNORE 5 ROWS;"
+mysql -h "130.207.36.76" SEREB_test -e "LOAD DATA INFILE '/var/lib/mysql-files/ecod.latest.csv' INTO TABLE SEREB.EcodDomains FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n' IGNORE 5 ROWS;"
