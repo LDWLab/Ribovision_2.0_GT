f8ce78e22ee05df1051419680bc98ba5977496bb
hmccann3 <hmccann3@gatech.edu>
2023-02-27 21:42:21 -0800

    Added vue tour, updated requirements (django 4)

--- static/js/components/RVGuide.vue
+++ static/js/components/RVGuide.vue
@@ -136,10 +136,9 @@
             },
         },
         mounted: function () {
-            //localStorage.setItem("hasCodeRunBefore", true)
+
             //vm.guideOff = false
-            /*
-            if (localStorage.getItem("hasCodeRunBefore") === null) {
+            if (!(localStorage.getItem("hasCodeRunBefore") == 'true')) {
                 tourSteps[0].content += '<br><b>First time users are advised to complete this guide by only clicking the Next button ▼</b>';
                 tourSteps.unshift(cookieNotice);
                 tourSteps[1].before = function before(type) {
@@ -152,7 +151,7 @@
                 }
                 this.$tours['myTour'].start();
             }
-            */
+            
         },
     }
 
